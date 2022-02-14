feebb681c716b46cb69d3f90a9756997c51b25e1
Prashanth Reddy <prashanth.abc369@gmail.com>
2022-02-13 21:28:18 -0500

    Added Error handling for graphql datafetch

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,17 +3,20 @@ import VehicleMakes from '../../database/models/VehicleMake'
 const fetchVehicleMakesFromDB = async (limit) => {
   let vehicleData = []
 
-  if (limit && limit > 0) {
-    vehicleData = await VehicleMakes.find()
-      .limit(limit)
-      .populate('vehicleTypes', 'typeId typeName')
-  } else {
-    vehicleData = await VehicleMakes.find().populate(
-      'vehicleTypes',
-      'typeId typeName'
-    )
+  try {
+    if (limit && limit > 0) {
+      vehicleData = await VehicleMakes.find()
+        .limit(limit)
+        .populate('vehicleTypes', 'typeId typeName')
+    } else {
+      vehicleData = await VehicleMakes.find().populate(
+        'vehicleTypes',
+        'typeId typeName'
+      )
+    }
+  } catch (err) {
+    throw Error(' Data could not be fetched', err.message)
   }
-
   return vehicleData
 }
 
